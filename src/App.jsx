78bfe58d079f3ca0f3590f076ab631ed5d9f1389
Drew-L-Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2023-06-14 22:39:33 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { Parallax } from 'react-parallax'
+
+export default function App() {
+  return (
+    <div className="App">
+        <Parallax strength={600}>
+            Hello World
+        </Parallax>
+
+        <Parallax>
+            Hello World
+        </Parallax>
+
+        <Parallax>
+            Hello World
+        </Parallax>
+    </div>
+  )
+}
