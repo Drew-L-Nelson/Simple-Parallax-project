f58e08de7ec254f092937d521396dbc8c2c3450d
Drew-L-Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2023-06-15 16:32:18 +0000

    App.css added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import './App.css'
 import { Parallax } from 'react-parallax'
+import CloudView from './Images/CloudView.jpg'
+import Landscape2 from './Images/Landscape2.jpg'
+import VaraMulti3 from './Images/VaraMulti3.jpg'
 
 export default function App() {
   return (
-    <div className="App">
-        <Parallax strength={600}>
-            Hello World
+    <div className='App'>
+        <Parallax strength={600} bgImage={Landscape2}>
+            <div className='content'>
+                <div className='text-content'>Normal Prallax</div>
+            </div>
         </Parallax>
 
         <Parallax>
-            Hello World
+            
         </Parallax>
 
         <Parallax>
-            Hello World
+            
         </Parallax>
     </div>
   )
