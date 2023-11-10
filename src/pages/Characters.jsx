cfac5d5967864e00ec3dcefe0e943af3a05785c1
Drew-L-Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2023-11-10 04:01:08 +0000

    Troubleshooitng overlay

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Landscape2 from '../Images/Landscape2.jpg'
 import '../styles/Characters.css';
 
 //rebuild this data fetching in Firebase after testing
@@ -39,6 +40,9 @@ const Characters = () => {
     
     return (
         <div className="characters-container">
+            <div className="characters-header-div" style={{ backgroundImage: `url(${Landscape2})` }}>
+                
+            </div>
             <div className="characters-scroll-container">
                 {characters.map(character => (
                     <CharacterCard key={character.id} character={character} onOpen={openBackstory}/>
